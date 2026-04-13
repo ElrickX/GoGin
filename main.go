e1bf0f02da8ca167658d34fa7bef59ea17137092
ElrickX <obsidianlabx@gmail.com>
2026-04-13 14:14:44 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,37 @@
+package main
+
+import (
+	"gin_demo/internal/handler"
+	"gin_demo/middleware"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+
+	_ "gin_demo/docs" // swag 生成的文件
+
+	httpSwagger "github.com/swaggo/http-swagger"
+)
+
+// ===== Swagger 基本信息 =====
+
+// @title Demo API
+// @version 1.0
+// @description Simple SaaS API with JWT Auth
+// @host localhost:8080
+// @BasePath /
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+// ===== MAIN =====
+
 func main() {
 
 	http.HandleFunc("/login", middleware.LoggingMiddleware(handler.LoginHandler))
