c8157c2b2422c419e6827d881a7fa1f2adeafff3
ElrickX <obsidianlabx@gmail.com>
2026-04-13 14:12:42 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,37 +1,3 @@
-package main
-
-import (
-	"log"
-	"net/http"
-
-	"gin_demo/internal/handler"
-	"gin_demo/middleware"
-
-	"github.com/golang-jwt/jwt/v5"
-
-	_ "gin_demo/docs" // swag 生成的文件
-
-	httpSwagger "github.com/swaggo/http-swagger"
-)
-
-// ===== Swagger 基本信息 =====
-
-// @title Demo API
-// @version 1.0
-// @description Simple SaaS API with JWT Auth
-// @host localhost:8080
-// @BasePath /
-// @securityDefinitions.apikey BearerAuth
-// @in header
-// @name Authorization
-
-type Claims struct {
-	Username string `json:"username"`
-	jwt.RegisteredClaims
-}
-
-// ===== MAIN =====
-
 func main() {
 
 	http.HandleFunc("/login", middleware.LoggingMiddleware(handler.LoginHandler))
@@ -43,14 +9,16 @@ func main() {
 	// Swagger UI
 	http.Handle("/swagger/", httpSwagger.WrapHandler)
 
-	port := ":8080"
+	// ✅ FIX HERE
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-	log.Println("🚀 Server started on http://localhost:8080")
-	log.Println("To view API Docs, refer to http://localhost:8080/swagger")
+	log.Println("🚀 Server started on port:", port)
 
-	err := http.ListenAndServe(port, nil)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
